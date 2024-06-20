40ffde5685025cc56af33cbfd4cead92130d8e91
Tre Halvorson <trehalvorson2@gmail.com>
2024-06-20 17:36:29 -0500

    Add health bar, more information for user

--- rpg.py
+++ rpg.py
@@ -1,48 +1,82 @@
 import random
+import math
 
 playerStats = { # [health, maxHealth]
-   "health": [100, 100],
+   "health": [15, 15],
    "damage": 1,
    "armor": 0,
    "level": 1,
    "xp": 0,
 
-   "weaponsEquipped": ["none", "none", "none"],
-   "armorEquipped": "none"
+   "weaponsEquipped": ["Fists", "Fists", "Fists"],
+   "armorEquipped": "Fists"
 }
 
 weapons = { # [damage, speed, fire, ice, poison]
-   "none": [0, 0, 0, 0, 0],
+   "Fists": [0, 0, 0, 0, 0],
    "sword": [10, 10, 0, 0, 0]
 }
 
+def healthBar(health, maxHealth):
+
+   for i in range(0, math.ceil(health / maxHealth * 20)):
+      print("=", end="")
+
+   for i in range(0, 20 - math.ceil(health / maxHealth * 20)):
+      print("-", end="")
+   
+   print("")
+
 def playerAttack(playerStats, enemyStats):
 
-   damage = playerStats["damage"] + weapons[playerStats["weaponsEquipped"][int(input("Which weapon would you like to use? 1, 2, or 3? ")) - 1]][0] + random.randint(0, 5)
+   print("Your weapons:")
+   for i in range(3):
+      print(f"{i + 1}. {playerStats['weaponsEquipped'][i]}")
+   chosenWeapon = playerStats["weaponsEquipped"][int(input("Which weapon would you like to use? 1, 2, or 3? ")) - 1]
+   damage = (playerStats["damage"] + weapons[chosenWeapon][0]) * random.uniform(1, 2)
 
    enemyStats["health"][0] -= damage
-   print(f"You dealt {damage} damage to the enemy. The enemy has {enemyStats['health'][0]} health left.")
+
+   print("")
+
+   if chosenWeapon == "Fists":
+      print("You punched the enemy!")
+   else:
+      print(f"You used a {chosenWeapon}!")
+
+   healthBar(enemyStats["health"][0], enemyStats["health"][1])
+   print(f"Enemy Health: {math.ceil(enemyStats['health'][0])}/{enemyStats['health'][1]}")
+   print("")
 
 def enemyAttack(playerStats, enemyStats):
 
    damage = enemyStats["damage"] + weapons[enemyStats["weaponEquipped"]][0] + random.randint(0, 5)
 
    playerStats["health"][0] -= damage
-   print(f"The enemy dealt {damage} damage to you. You have {playerStats['health'][0]} health left.")
+
+   print("")
+
+   if enemyStats["weaponEquipped"] == "Fists":
+      print("The enemy punched you!")
+   else:
+      print(f"The enemy used a {enemyStats['weaponEquipped']}!")
+   
+   healthBar(playerStats["health"][0], playerStats["health"][1])
+   print(f"Player Health: {math.ceil(playerStats['health'][0])}/{playerStats['health'][1]}")
 
 def attack(playerStats, enemyStats):
+
    playerAttack(playerStats, enemyStats)
    enemyAttack(playerStats, enemyStats)
 
 enemyStats = {
+   "species/name": "Slime",
    "health": [15, 15],
    "damage": 1,
    "armor": 0,
 
-   "weaponEquipped": "none",
-   "armorEquipped": "none",
+   "weaponEquipped": "Fists",
+   "armorEquipped": "Fists",
 }
 
-print("")
 attack(playerStats, enemyStats)
-print("")
